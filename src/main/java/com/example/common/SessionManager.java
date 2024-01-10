85872c55007a2b37bda580c98d4db0c15eb3a6e2
johnny-gill <uint777@gmail.com>
2024-01-10 11:14:20 +0900

    session 테스트

---
+++ src/main/java/com/example/common/SessionManager.java
@@ -0,0 +1,56 @@
+package com.example.common;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class SessionManager {
+
+    // session cookie : {mySessionId: uuid}
+    // session : {uuid: obj}
+
+    public static final String SESSION_COOKIE_NAME = "mySessionId";
+    private final Map<String, Object> sessionStore = new ConcurrentHashMap<>();
+
+    public void createSession(Object value, HttpServletResponse response) {
+        String sessionId = UUID.randomUUID().toString();
+        sessionStore.put(sessionId, value);
+
+        Cookie cookie = new Cookie(SESSION_COOKIE_NAME, sessionId);
+        response.addCookie(cookie);
+    }
+
+    public Object getSession(HttpServletRequest request) {
+        Cookie sessionCookie = findCookie(request, SESSION_COOKIE_NAME);
+        if (sessionCookie == null) {
+            return null;
+        }
+
+        return sessionStore.get(sessionCookie.getValue());
+    }
+
+    private Cookie findCookie(HttpServletRequest request, String cookieName) {
+        if (request.getCookies() == null) {
+            return null;
+        }
+
+        return Arrays.stream(request.getCookies())
+                .filter(cookie -> cookie.getName().equals(cookieName))
+                .findAny()
+                .orElse(null);
+    }
+
+    public void expire(HttpServletRequest request) {
+        Cookie sessionCookie = findCookie(request, SESSION_COOKIE_NAME);
+        if (sessionCookie != null) {
+            sessionStore.remove(sessionCookie.getValue());
+        }
+    }
+}
