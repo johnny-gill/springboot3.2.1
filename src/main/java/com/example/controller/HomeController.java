c80fcae56793141590aafb3ce81ec4a67a06355a
johnny-gill <uint777@gmail.com>
2024-01-18 16:10:53 +0900

    @SessionAttribute

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Slf4j
 @Controller
@@ -19,18 +20,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String home(Model model, HttpServletRequest request) {
-        HttpSession session = request.getSession(false);
-        if (session == null) {
+    public String home(Model model, @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember) {
+        if (loginMember == null) {
             return "home";
         }
-
-        Member member = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
-        if (member == null) {
-            return "home";
-        }
-
-        model.addAttribute("member", member);
+        model.addAttribute("member", loginMember);
         return "loginHome";
     }
 }
