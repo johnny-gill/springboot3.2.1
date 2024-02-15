1aabe1c840e0cb649a53fd81027d6a3d091407c3
johnny-gill <uint777@gmail.com>
2024-02-15 17:56:38 +0900

    request에서 에러 정보 확인

--- src/main/java/com/example/controller/exception/ErrorPageController.java
+++ src/main/java/com/example/controller/exception/ErrorPageController.java
@@ -1,5 +1,7 @@
 package com.example.controller.exception;
 
+import jakarta.servlet.DispatcherType;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,15 +12,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/error-page")
 public class ErrorPageController {
 
+    public static final String ERROR_EXCEPTION = "jakarta.servlet.error.exception";
+    public static final String ERROR_EXCEPTION_TYPE = "jakarta.servlet.error.exception_type";
+    public static final String ERROR_MESSAGE = "jakarta.servlet.error.message";
+    public static final String ERROR_REQUEST_URI = "jakarta.servlet.error.request_uri";
+    public static final String ERROR_SERVLET_NAME = "jakarta.servlet.error.servlet_name";
+    public static final String ERROR_STATUS_CODE = "jakarta.servlet.error.status_code";
+
     @GetMapping("/404")
-    public String errorPage404() {
-        log.info("errorPage404");
+    public String errorPage404(HttpServletRequest request) {
+        printErrorInfo(request);
         return "error-page/404";
     }
 
     @GetMapping("/500")
-    public String errorPage500() {
-        log.info("errorPage500");
+    public String errorPage500(HttpServletRequest request) {
+        printErrorInfo(request);
         return "error-page/500";
+    }
+
+    private void printErrorInfo(HttpServletRequest request) {
+        log.info("ERROR_EXCEPTION: ", request.getAttribute(ERROR_EXCEPTION));
+        log.info("ERROR_EXCEPTION_TYPE: {}", request.getAttribute(ERROR_EXCEPTION_TYPE));
+        log.info("ERROR_MESSAGE: {}", request.getAttribute(ERROR_MESSAGE));
+        log.info("ERROR_REQUEST_URI: {}", request.getAttribute(ERROR_REQUEST_URI));
+        log.info("ERROR_SERVLET_NAME: {}", request.getAttribute(ERROR_SERVLET_NAME));
+        log.info("ERROR_STATUS_CODE: {}", request.getAttribute(ERROR_STATUS_CODE));
+        log.info("DispatcherType: {}", request.getDispatcherType());
     }
 }
--- src/main/java/com/example/controller/exception/ServletExceptionController.java
+++ src/main/java/com/example/controller/exception/ServletExceptionController.java
@@ -1,7 +1,9 @@
 package com.example.controller.exception;
 
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jdk.jfr.Frequency;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
