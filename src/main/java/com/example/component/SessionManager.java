9df1a9147ff82c8c21395e43d05a16f85c751313
johnny-gill <uint777@gmail.com>
2024-02-16 14:09:47 +0900

    미ì사용 component 주석

--- src/main/java/com/example/component/SessionManager.java
+++ src/main/java/com/example/component/SessionManager.java
@@ -10,7 +10,7 @@ import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
-@Component
+//@Component
 public class SessionManager {
 
     public static final String SESSION_COOKIE_NAME = "mySessionId";
