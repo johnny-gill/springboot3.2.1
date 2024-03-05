4da033b2b5717825da55e440c6fb62a773f9295b
johnny-gill <uint777@gmail.com>
2024-03-05 14:43:59 +0900

    conform에 erter ì r 적용

--- src/main/java/com/example/controller/converter/ConverterController.java
+++ src/main/java/com/example/controller/converter/ConverterController.java
@@ -1,9 +1,12 @@
 package com.example.controller.converter;
 
 import com.example.domain.type.IpPort;
+import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ConverterController {
@@ -13,5 +16,28 @@ public class ConverterController {
         model.addAttribute("number", 10000);
         model.addAttribute("ipPort", new IpPort("127.0.0.1", 9000));
         return "converter/converter-view";
+    }
+
+    @GetMapping("/converter/edit")
+    public String converterForm(Model model) {
+        Form form = new Form(new IpPort("127.0.0.1", 55555));
+        model.addAttribute("form", form);
+        return "converter/converter-form";
+    }
+
+    @PostMapping("/converter/edit")
+    public String converterEdit(@ModelAttribute Form form, Model model) {
+        IpPort ipPort = form.getIpPort();
+        model.addAttribute("ipPort", ipPort);
+        return "converter/converter-view";
+    }
+
+    @Data
+    static class Form {
+        private IpPort ipPort;
+
+        public Form(IpPort ipPort) {
+            this.ipPort = ipPort;
+        }
     }
 }
