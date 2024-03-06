eace8f24cf28e79592c5b7ccfd2fef637b965d2b
johnny-gill <uint777@gmail.com>
2024-03-06 10:39:41 +0900

    spring formmater

---
+++ src/main/java/com/example/controller/formatter/FormatterController.java
@@ -0,0 +1,46 @@
+package com.example.controller.formatter;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.NumberFormat;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Slf4j
+@Controller
+public class FormatterController {
+
+    @GetMapping("/formatter/edit")
+    public String formatterForm(Model model) {
+
+        Form form = new Form();
+        form.setNumber(10000);
+        form.setLocalDateTime(LocalDateTime.now());
+
+        model.addAttribute("form", form);
+        return "formatter/formatter-form";
+    }
+
+    @PostMapping("/formatter/edit")
+    public String formmaterEdit(@ModelAttribute Form form, Model model) {
+        log.info("form={}, model={}", form, model);
+        return "formatter/formatter-view";
+    }
+
+    @Data
+    static class Form {
+
+        @NumberFormat(pattern = "###,###")
+        private Integer number;
+
+        @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        private LocalDateTime localDateTime;
+    }
+}
